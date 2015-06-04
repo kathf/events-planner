1f00354633892cdb949c2e6297d248bcfc6a4dc5
Kath <kathf@hotmail.com>
2015-06-04 15:05:03 +1000

    events controller and view created

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   has_many :tasks
+  validates :name, :start, :end, presence: true
 end
--- db/migrate/20150604020858_create_contacts.rb
+++ db/migrate/20150604020858_create_contacts.rb
@@ -2,8 +2,8 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
-      t.string :phone, default: []
-      t.string :address, default: []
+      t.string :phone, array: true, default: []
+      t.string :address, array: true, default: []
       t.string :email
       t.text :comment
       t.timestamps null: false
